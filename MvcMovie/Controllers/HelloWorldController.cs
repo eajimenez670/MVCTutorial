8da8dc57f4c889cc7ce5021fd7c9576d6c61ffee
DIGITALWARE\EdisonJ <edisonj@digitalware.com.co>
2022-07-15 18:35:23 -0500

    First commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /// <summary>
+        /// Get: /HelloWorld/
+        /// </summary>
+        /// <returns></returns>
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        /// <summary>
+        /// Get: /HelloWorld/Welcome/
+        /// </summary>
+        /// <returns></returns>
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
